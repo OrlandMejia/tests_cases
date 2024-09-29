d80925871c26d0b54854cad078f23124a93743e3
Orlando <orlandmejia374@gmail.com>
2024-09-28 20:28:49 -0600

    guardar datos y get al server

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.post("/create", (req, res) => {
     const fecha_creacion = req.body.fecha_creacion;
 
     db.query('INSERT INTO test_case(codigo, nombre, descripcion, nivel_importancia, nombre_proyecto, id_usuario_asignado, fecha_asignacion) VALUES (?, ?, ?, ?, ?, ?, ?)',
-        [codigo, nombre, descripcion, nivel_importancia, nombre_proyecto, id_usuario_asignado, fecha_creacion], // Asegurarse de incluir nombre_proyecto en la consulta
+        [codigo, nombre, descripcion, nivel_importancia, nombre_proyecto, id_usuario_asignado, fecha_creacion], // datos que enviaremos
         (err, result) => {
             if (err) {
                 console.log(err);
@@ -38,6 +38,34 @@ app.post("/create", (req, res) => {
         }
     );
 });
+
+// peticion para mostrar a los usuarios para asignarlos
+// Obtener usuarios con rol QA
+app.get("/usuarios-qa", (req, res) => {
+    db.query("SELECT idUsuario as id, nombre FROM users WHERE rol = 'qa'", (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error en la base de datos");
+        } else {
+            res.send(result); // Enviar la lista de usuarios
+        }
+    });
+});
+
+// metodo para listar  los casos de prueba
+app.get("/casos", (req, res) => {
+
+    db.query('SELECT * FROM  test_case', (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+
 
 //decimos que vamos a escuchar por un puerto
 app.listen(3001, () => { 
